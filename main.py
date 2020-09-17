57f8f31131ddd7b6f7fbbbdd49d7258c1484f2b2
junyangGuan <70017096+junyangGuan@users.noreply.github.com>
2020-09-17 13:54:14 +0000

    save

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
 def digit_sum(n):
-  a = n%10
-  b = n//10%10
-  c = n//100%10
-  d = n//1000%10
-  e = n//10000%10
-  f = n//100000%10
-  g = n//1000000%10
-  h = n//10000000%10
-  i = n//100000000%10
-  j = n//1000000000%10
-  sum = a+b+c+d+e+f+g+h+i+j
-  a=int(a)
-  b=int(b)
-  c=int(c)
-  d=int(d)
-  e=int(e)
-  f=int(f)
-  g=int(g)
-  h=int(h)
-  i=int(i)
-  j=int(j)
-  sum=int(sum)
-  return sum
+  if n//10 > 0 :
+    return   digit_sum(n//10) + n%10
+  else :
+    return  n%10
+  
 def run():
   cj = input("Enter an int: ")
   cj = int(cj)
   print(f"sum of digits of {cj} is {digit_sum(cj)}.")
-if __name__ =="__main__":
+if __name__  == "__main__":
   run()
